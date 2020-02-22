8d203ee641c3ea323d6d145fe9701a47496867db
tommach <tomek.machalewski@gmail.com>
2020-02-22 14:10:05 +0100

    Added descriptive BMI

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -10,8 +10,14 @@ public class Main {
         System.out.println("What is your height in m?");
         float height = sc.nextFloat();
         float bmi=bmi(weight, height);
-        System.out.println(bmi);
+        System.out.println("Your bmi is "+bmi+"that quallifies as "+bmiDescriptive(bmi));
 
+    }
+
+    private static String bmiDescriptive(float bmi) {
+        if(bmi<18.5) return "Underweight";
+        else if(bmi<25) return "Normal (healthy weight)";
+        else return "Overweight";
     }
 
     private static float bmi(float weight, float height) {
