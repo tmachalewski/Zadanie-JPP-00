534403271451763b65b507688f3f9e5b2f278504
tommach <tomek.machalewski@gmail.com>
2020-02-22 13:35:59 +0100

    Added BMI calculation

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -1,7 +1,20 @@
 package pl.sda;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your weight in kg?");
+        float weight = sc.nextFloat();
+        System.out.println("What is your height in m?");
+        float height = sc.nextFloat();
+        float bmi=bmi(weight, height);
+        System.out.println(bmi);
 
+    }
+
+    private static float bmi(float weight, float height) {
+        return weight/(height*height);
     }
 }
